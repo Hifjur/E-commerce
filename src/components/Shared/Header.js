abc9d322ca3f9c4ab6e593d10b1f3aa244980049
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-02 00:46:07 +0600

    add

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+import { Container, FloatingLabel, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#"><i className="fab fa-etsy"></i>-commerce</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+              <div className='header mt-2'>
+                    <div>
+                        <Navbar.Brand href="#"><i className="fab fa-etsy"></i><span className='commerce'>-commerce</span></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                    </div>
 
-                     <Form className="d-flex header_from">
-                       <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="w-100 ms-2"
-                            aria-label="Search"/>
-                        <button type="button" className="btn btn-primary w-25">
-                            <i className="fas fa-search"></i>
-                        </button>
-                        {/* <Button className='header_button'>Search</Button> */}
-                    
-                    
-                     {/*<div className="input-group">
-                        <div className="form-outline">
-                            <input type="search" id="form1" className="form-control" />
-                            <label className="form-label" for="form1">Search</label>
-                        </div>
-                        <button type="button" className="btn btn-primary">
-                            <i className="fas fa-search"></i>
-                        </button>
-                    </div>*/}
- </Form>    
-
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px', marginLeft: '670px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1"><i className="fas fa-layer-group"></i></Nav.Link>
-                        <Nav.Link href="#action1"><i className="fas fa-heart"></i></Nav.Link>
-                        <Nav.Link href="#action1"><i className="fas fa-user"></i></Nav.Link>
-                        <Nav.Link href="#action1"><i className="fas fa-shopping-cart"></i></Nav.Link>
+                    <div>
+                        <Form className="d-flex header_from">
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="form_button ms-2"
+                                aria-label="Search" />
                         
-                    </Nav>
-                    
-                </Navbar.Collapse>
+                                
+                            <button className="w-25 header_button">
+                                <i className="fas fa-search"></i>
+                            </button>
+                        </Form>
+
+                    </div>
+
+                    <div>
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1"><i className="fas fa-layer-group"></i></Nav.Link>
+                            <Nav.Link href="#action1"><i className="fas fa-heart"></i></Nav.Link>
+                            <Nav.Link href="#action1"><i className="fas fa-user"></i></Nav.Link>
+                            <Nav.Link href="#action1"><i className="fas fa-shopping-cart"></i></Nav.Link>
+
+                        </Nav>
+                    </div>
+
+              </div>
+                
             </Container>
         </Navbar>
     );
