4efd476a8515c884f83273ddc2cebba16f222e41
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-02 23:53:35 +0600

    add

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Slide from '../Slide/Slide';
 import Banner from './../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Slide></Slide>
         </div>
     );
 };
