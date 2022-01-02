25a1326e8433bc0b1096fc8038264bdb45f4c4e4
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-02 21:19:44 +0600

    add

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { Carousel } from 'react-carousel-minimal';
-<<<<<<< HEAD
-
-
-=======
->>>>>>> shared
 
 const Banner = () => {
     const data = [
@@ -17,11 +12,7 @@ const Banner = () => {
         },
         {
             image:
-<<<<<<< HEAD
-                "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-=======
                 "https://i.ibb.co/7bXypsB/s-1.png",
->>>>>>> shared
             caption: "<div>San Francisco</div>",
         },
         {
@@ -64,11 +55,6 @@ const Banner = () => {
     const captionStyle = {
         fontSize: "2em",
         fontWeight: "bold",
-<<<<<<< HEAD
-       
-=======
-
->>>>>>> shared
     };
     const slideNumberStyle = {
         fontSize: "20px",
@@ -86,11 +72,7 @@ const Banner = () => {
                         data={data}
                         time={2000}
                         width="100%"
-<<<<<<< HEAD
-                        height="400px"
-=======
                         height="500px"
->>>>>>> shared
                         captionStyle={captionStyle}
                         radius="10px"
                         slideNumber={true}
@@ -109,11 +91,6 @@ const Banner = () => {
                             textAlign: "center",
                             maxWidth: "1250px",
                             margin: "40px auto",
-<<<<<<< HEAD
-                            
-=======
-
->>>>>>> shared
                         }}
                     />
                 </div>
