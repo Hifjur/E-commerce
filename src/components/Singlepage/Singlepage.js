2c18b5393a5ed213136bd78fc8a24c153a73d33d
Tushar <freelancerrtushar@gmail.com>
2022-01-03 09:52:43 +0600

    related product part

--- src/components/Singlepage/Singlepage.js
+++ src/components/Singlepage/Singlepage.js
@@ -1,6 +1,7 @@
-import { Box, Button, Container, Grid } from '@mui/material'
-import React,{useState} from 'react'
-import stylesinglepage from './singlepage.module.css'
+import { Box, Button, ButtonGroup, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import Magnifier from "react-magnifier";
+import './singlepage.css'
 
 
 function Singlepage() {
@@ -15,7 +16,8 @@ function Singlepage() {
             "https://i.postimg.cc/g0Gv4jm0/ladis4.jpg"
         ],
         "description":"Mes soir archipels plus juillets j'ai lors. Horribles lâche poeme plus choient cinquante pourrit entonnoirs, pareils de par que maries cieux ô yeux roulis, les un d'oiseaux le sidéraux au en les, crouler ont porteurs qu'un chair dévorés ni pas et que, montait j'ai plus noyé atroce aux avec ardents a quand, mes chantants fleurs anglais ni comme des aux des regretter. Dorades sous les l'horizon a et archipels monté des. Que que haleurs courus au le, d'eau qu'un peuple dans reculons montrer cotons troupeaux aux monitors. Perdu le les éclate les de que plus. Écumes illuminant courus subi les pareils monté des, éternels éternel avec crouler les taché de. Ainsi un lorsqu'a pontons sans, rouleurs mer les punaises exquise, le au d'horreurs l'aube or. Des de que plus j'aurais vastes. Coup ameres punaises courus fermentent montrer un trombes dont phosphores, jaunes puis plus.",
-        "price":"1000"
+        "price": "1000",
+        "category":"ladies"
     })
     
     return (
@@ -23,11 +25,9 @@ function Singlepage() {
             {/* singleproduct */}
             <Grid container spacing={2} sx={{paddingBottom:"15px",paddingRight:"15px",borderRadius:"10px"}}>
                 <Grid item md={5} sm={6} xs={12} >
-                    
-                    <img src={product.src[indexNumber]} width="100%" height="500px" style={{borderRadius:"5px"}} />
-                    
-                    
+                    <Magnifier src={product.src[indexNumber]} width="100%" height="500px"  style={{ borderRadius: "5px", boxShadow: "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px" }} />
                 </Grid>
+
                 <Grid item md={7} xs={12} sm={6}>
                     <Box sx={{ display: "flex", justifyContent: 'space-between'}}>
                         <h2>{product.title}</h2>
@@ -47,6 +47,82 @@ function Singlepage() {
                 </Grid>
             </Grid> 
             {/* singleproduct end */}
+            <h2 style={{ backgroundColor:"#bcbbff",padding:"15px 15px",borderRadius:"5px", marginBottom:"15px"}}>Realated product</h2>
+            <Grid container spacing={2}>
+               
+                <Grid item md={3} sm={6} xs={6}>
+                    <Card className="mycard" sx={{}}>
+                        <img src="https://i.postimg.cc/ydzNX7Wj/dress1.jpg" width="100%" height="300px"/>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                         
+                        </CardContent>
+                        <CardActions>
+                            <ButtonGroup disableElevation variant="contained" style={{ width: "100%" }}>
+                                <Button variant="contained" style={{ width: "50%", }}>Details</Button>
+                                <Button variant="contained" color="secondary" style={{ width: "50%" }}>Add to cart</Button>
+                            </ButtonGroup>
+                        </CardActions>
+                    </Card> 
+                </Grid>
+
+                <Grid item md={3} sm={6} xs={6}>
+                    <Card className="mycard" sx={{}}>
+                        <img src="https://i.postimg.cc/CL4Qs0LN/ladies-dresses.jpg" width="100%" height="300px" />
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            
+                        </CardContent>
+                        <CardActions>
+                            <ButtonGroup disableElevation variant="contained" style={{ width: "100%" }}>
+                                <Button variant="contained" style={{ width: "50%", }}>Details</Button>
+                                <Button variant="contained" color="secondary" style={{ width: "50%" }}>Add to cart</Button>
+                            </ButtonGroup>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid item md={3} sm={6} xs={6} >
+                    <Card className="mycard">
+                        <img src="https://i.postimg.cc/ydzNX7Wj/dress1.jpg" width="100%" height="300px"/>
+                        <CardContent>
+                            <Typography variant="h5" >
+                                Dress one
+                            </Typography>
+                            
+                        </CardContent>
+                        <CardActions>
+                            <ButtonGroup disableElevation variant="contained" style={{width:"100%"}}>
+                                <Button variant="contained" style={{ width: "50%",}}>Details</Button>
+                                <Button variant="contained" color="secondary" style={{ width: "50%"}}>Add to cart</Button>
+                            </ButtonGroup>
+                         </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid item md={3} sm={6} xs={6}>
+                    <Card className="mycard" sx={{}}>
+                        <img src="https://i.postimg.cc/g0Gv4jm0/ladis4.jpg" width="100%" height="300px" />
+                        <CardContent>
+                            <Typography  variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            
+                        </CardContent>
+                        <CardActions>
+                            <ButtonGroup disableElevation variant="contained" style={{ width: "100%" }}>
+                                <Button variant="contained" style={{ width: "50%", }}>Details</Button>
+                                <Button variant="contained" color="secondary" style={{ width: "50%" }}>Add to cart</Button>
+                            </ButtonGroup>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                
+            </Grid>
         </Container>
     )
 }
