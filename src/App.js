dea2d0cd2a37e79935f440c2a9daa8c76af96bc1
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-03 17:51:05 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Shared/Header";
 import Home from "./components/Home/Home/Home";
-
 import Login from "./components/Authentication/Login";
 import AuthProvider from "./AuthProvider/AuthProvider";
 import Register from "./components/Authentication/Register";
-=======
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import './App.css';
-import Header from './components/Shared/Header';
-import Home from './components/Home/Home/Home';
 import Footer from "./components/Shared/Footer/Footer";
->>>>>>> shared
 
 function App() {
   return (
@@ -25,25 +13,15 @@ function App() {
       <BrowserRouter>
         <AuthProvider>
           <Header></Header>
-<<<<<<< HEAD
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-          </Routes>
+          </Routes>  
+          <Footer></Footer>
         </AuthProvider>
       </BrowserRouter>
-      ,
-=======
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-           
-        </Routes>
-        <Footer></Footer>
-      </BrowserRouter>,
->>>>>>> shared
     </div>
   );
 }
