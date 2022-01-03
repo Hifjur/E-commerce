cc0d14f6a57d305b47feba5eefc75878bd7fe10b
Tushar <freelancerrtushar@gmail.com>
2022-01-03 17:55:35 +0600

    some changes

--- src/Checkoutpage/Billingsection.js
+++ src/Checkoutpage/Billingsection.js
@@ -1,18 +1,57 @@
 import React,{useState} from 'react'
-import { Box, Button, ButtonGroup, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Box, Button, ButtonGroup, Card, CardActions, CardContent, CardMedia, Container, FormControl, Grid, Typography } from '@mui/material'
+import TextField from '@mui/material/TextField';
 function Billingsection() {
-    const [pageoption, setPageoptoin] = useState(false);
+    const [pageoption, setPageoption] = useState(true);
+    
 
     return (
-        <Box>
-            <form>
-                <input type="text" name="username" />
-                <input type="text" name="address" />
-                <input type="text" name="city" />
-                <input type="text" name="phonenumber" />
-                <input type="text" name="postalcode"/>
-           </form>
-        </Box>
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+               
+                  <Box>
+                    <Typography variant='h4' style={{ backgroundColor: "#bcbbff", padding: "8px 15px", borderRadius: "5px"}}>
+                        {
+                            pageoption ?"Billing information":"Payment information"  
+                        } 
+                    </Typography>
+                    <form>
+                        {
+                            pageoption ? <>
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <TextField id="outlined-basic" name="username" label="User name" variant="outlined" />
+                                </FormControl>
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <TextField id="outlined-basic" name="address" label="Address" variant="outlined" />
+                                </FormControl>
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <TextField id="outlined-basic" name="city" label="City name" variant="outlined" />
+                                </FormControl>
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <TextField id="outlined-basic" name="phonenumber" label="Phonenumber" variant="outlined" />
+                                </FormControl>
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <TextField id="outlined-basic" name="postalcode" label="Postalcode" variant="outlined" />
+                                </FormControl>
+
+                                <FormControl fullWidth style={{ margin: "10px 0px" }} variant="standard">
+                                    <Button variant='contained' onClick={() =>setPageoption(false)}>Next</Button>
+                                </FormControl>
+                            </>:"gfhfghg"
+                                
+                        }
+                       
+
+
+                    </form>
+                </Box>
+                </Grid>
+                <Grid item xs={4}>
+                    
+                </Grid>
+                
+                </Grid>
+        
     )
 }
 
