592984b57e874deb8a3afa3b121f0deca7fd860f
HIFJUR RAHMAN <hifjur1313@gmail.com>
2022-01-04 13:37:37 +0600

    payment setup

--- src/Payment/Payment.js
+++ src/Payment/Payment.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
-function Payment() {
-    return (
-        <div>
-            <h1>payment</h1>
-        </div>
-    )
-}
+const stripePromise = loadStripe(
+  "pk_test_51Jx5DqJ8BbjJfr4C3I3MZcKJ6w8ahM4uSsZBCJgOivywGZqcDrayV4LsfZoGvy9womiNLKFhj0vN9lYyfqYecNu300vgw93X9a"
+);
 
-export default Payment
+const Payment = () => {
+  const { id } = useParams();
+  const [bookings, setBookings] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBookings(data);
+      });
+  }, [id]);
+  return (
+    <div
+      style={{
+        paddingTop: "120px",
+        paddingBottom: "120px",
+        backgroundColor: "#1D2440",
+        color: "white",
+        borderRadius: 20,
+      }}
+    >
+      <img
+        style={{ border: "6px solid white", borderRadius: 20 }}
+        src={bookings.img}
+        alt=""
+      />
+      <h1>{bookings.hotelName} </h1>
+      <h2>Rent ${bookings.rent}</h2>
+
+      {bookings.rent && (
+        <Elements stripe={stripePromise}>
+          <CheckoutForm bookings={bookings}></CheckoutForm>
+        </Elements>
+      )}
+      <p style={{ color: "gray", fontSize: 20 }}>
+        Test Card Number for Demo only 4242424242424242
+      </p>
+    </div>
+  );
+};
+
+export default Payment;
